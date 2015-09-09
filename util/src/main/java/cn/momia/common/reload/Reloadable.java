23a5f39a7f2f46e312e2ac62d661d2c67ec0b6fb
吴秋锋 <arthas.wuqf@gmail.com>
2015-09-09 10:31:58 +0800

    添加Reloadable接口

---
+++ util/src/main/java/cn/momia/common/reload/Reloadable.java
@@ -0,0 +1,30 @@
+package cn.momia.common.reload;
+
+import java.util.Date;
+
+public abstract class Reloadable {
+    private Date lastReloadTime = null;
+    private int reloadIntervalMinutes = 24 * 60;
+
+    public void setReloadIntervalMinutes(int reloadIntervalMinutes) {
+        this.reloadIntervalMinutes = reloadIntervalMinutes;
+    }
+
+    protected synchronized void reload() {
+        if (isOutOfDate()) return;
+
+        try {
+            doReload();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lastReloadTime = new Date();
+        }
+    }
+
+    protected boolean isOutOfDate() {
+        return lastReloadTime == null || lastReloadTime.before(new Date(new Date().getTime() - reloadIntervalMinutes * 60 * 1000));
+    }
+
+    protected abstract void doReload();
+}
--- webapp/src/main/java/cn/momia/common/service/DbAccessService.java
+++ webapp/src/main/java/cn/momia/common/service/DbAccessService.java
@@ -1,11 +1,15 @@
 package cn.momia.common.service;
 
+import cn.momia.common.reload.Reloadable;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-public abstract class DbAccessService {
+public abstract class DbAccessService extends Reloadable {
     protected JdbcTemplate jdbcTemplate;
 
     public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
+
+    @Override
+    protected void doReload() {}
 }
