ea4d62f08def4b50c853725687a0c91e0df33116
吴秋锋 <arthas.wuqf@gmail.com>
2015-09-10 15:36:02 +0800

    事务

--- webapp/src/main/java/cn/momia/common/service/DbAccessService.java
+++ webapp/src/main/java/cn/momia/common/service/DbAccessService.java
@@ -2,12 +2,18 @@ package cn.momia.common.service;
 
 import cn.momia.common.reload.Reloadable;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.support.TransactionTemplate;
 
 public abstract class DbAccessService extends Reloadable {
     protected JdbcTemplate jdbcTemplate;
+    protected TransactionTemplate transactionTemplate;
 
     public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void setTransactionTemplate(TransactionTemplate transactionTemplate) {
+        this.transactionTemplate = transactionTemplate;
     }
 
     @Override
