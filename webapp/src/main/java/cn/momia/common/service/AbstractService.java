734a50cc747bb1f55adade87cc9ac8acd7eefc83
吴秋锋 <arthas.wuqf@gmail.com>
2015-11-13 13:16:57 +0800

    jdbc template 设为private

--- webapp/src/main/java/cn/momia/common/service/AbstractService.java
+++ webapp/src/main/java/cn/momia/common/service/AbstractService.java
@@ -3,15 +3,16 @@ package cn.momia.common.service;
 import cn.momia.common.reload.Reloadable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowCallbackHandler;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.transaction.support.TransactionCallback;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -23,8 +24,8 @@ public abstract class AbstractService extends Reloadable {
 
     private static Map<String, Map<String, Method>> classSetterMethods = new HashMap<String, Map<String, Method>>();
 
-    protected JdbcTemplate jdbcTemplate;
-    protected TransactionTemplate transactionTemplate;
+    private JdbcTemplate jdbcTemplate;
+    private TransactionTemplate transactionTemplate;
 
     public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
@@ -37,13 +38,13 @@ public abstract class AbstractService extends Reloadable {
     @Override
     protected void doReload() {}
 
+    public int queryInt(String sql) {
+        return queryInt(sql, null);
+    }
+
     public int queryInt(String sql, Object[] args) {
-        return jdbcTemplate.query(sql, args, new ResultSetExtractor<Integer>() {
-            @Override
-            public Integer extractData(ResultSet rs) throws SQLException, DataAccessException {
-                return rs.next() ? rs.getInt(1) : 0;
-            }
-        });
+        List<Integer> results = queryIntList(sql, args);
+        return results.isEmpty() ? 0 : results.get(0);
     }
 
     public List<Integer> queryIntList(String sql) {
@@ -54,13 +55,13 @@ public abstract class AbstractService extends Reloadable {
         return jdbcTemplate.queryForList(sql, args, Integer.class);
     }
 
+    public long queryLong(String sql) {
+        return queryLong(sql, null);
+    }
+
     public long queryLong(String sql, Object[] args) {
-        return jdbcTemplate.query(sql, args, new ResultSetExtractor<Long>() {
-            @Override
-            public Long extractData(ResultSet rs) throws SQLException, DataAccessException {
-                return rs.next() ? rs.getLong(1) : 0;
-            }
-        });
+        List<Long> results = queryLongList(sql, args);
+        return results.isEmpty() ? 0 : results.get(0);
     }
 
     public List<Long> queryLongList(String sql) {
@@ -71,33 +72,49 @@ public abstract class AbstractService extends Reloadable {
         return jdbcTemplate.queryForList(sql, args, Long.class);
     }
 
+    public String queryString(String sql, Object[] args, String defaultValue) {
+        List<String> results = queryStringList(sql, args);
+        return results.isEmpty() ? defaultValue : results.get(0);
+    }
+
     public List<String> queryStringList(String sql, Object[] args) {
         return jdbcTemplate.queryForList(sql, args, String.class);
     }
 
-    public String queryString(String sql, Object[] args, String defaultValue) {
-        try {
-            return jdbcTemplate.queryForObject(sql, args, String.class);
-        } catch (Exception e) {
-            return defaultValue;
-        }
+    public Date queryDate(String sql, Object[] args, Date defaultValue) {
+        List<Date> results = queryDateList(sql, args);
+        return results.isEmpty() ? defaultValue : results.get(0);
     }
 
-    public Date queryDate(String sql, Object[] args, Date defaultValue) {
-        try {
-            return jdbcTemplate.queryForObject(sql, args, Date.class);
-        } catch (Exception e) {
-            return defaultValue;
-        }
+    public List<Date> queryDateList(String sql, Object[] args) {
+        return jdbcTemplate.queryForList(sql, args, Date.class);
+    }
+
+    public <T> T queryObject(String sql, Class<T> clazz, T defaultValue) {
+        return queryObject(sql, null, clazz, defaultValue);
     }
 
     public <T> T queryObject(String sql, Object[] args, Class<T> clazz, T defaultValue) {
-        try {
-            Map<String, Object> row = jdbcTemplate.queryForMap(sql, args);
-            return buildObject(row, clazz);
-        } catch (Exception e) {
-            return defaultValue;
+        List<T> objects = queryObjectList(sql, args, clazz);
+        return objects.isEmpty() ? defaultValue : objects.get(0);
+    }
+
+    public <T> List<T> queryObjectList(String sql, Class<T> clazz) {
+        return queryObjectList(sql, null, clazz);
+    }
+
+    public <T> List<T> queryObjectList(String sql, Object[] args, Class<T> clazz) {
+        List<Map<String, Object>> rows = jdbcTemplate.queryForList(sql, args);
+        List<T> list = new ArrayList<T>();
+        for (Map<String, Object> row : rows) {
+            try {
+                list.add(buildObject(row, clazz));
+            } catch (Exception e) {
+                LOGGER.error("invalid row value: {}", row, e);
+            }
         }
+
+        return list;
     }
 
     private <T> T buildObject(Map<String, Object> row, Class<T> clazz) throws IllegalAccessException, InstantiationException, InvocationTargetException {
@@ -111,24 +128,6 @@ public abstract class AbstractService extends Reloadable {
         }
 
         return t;
-    }
-
-    public <T> List<T> queryList(String sql, Class<T> clazz) {
-        return queryList(sql, new Object[] {}, clazz);
-    }
-
-    public <T> List<T> queryList(String sql, Object[] args, Class<T> clazz) {
-        List<Map<String, Object>> rows = jdbcTemplate.queryForList(sql, args);
-        List<T> list = new ArrayList<T>();
-        for (Map<String, Object> row : rows) {
-            try {
-                list.add(buildObject(row, clazz));
-            } catch (Exception e) {
-                LOGGER.error("invalid row value: {}", row, e);
-            }
-        }
-
-        return list;
     }
 
     private <T> Map<String, Method> getSetterMethods(Class<T> clazz) {
@@ -152,7 +151,34 @@ public abstract class AbstractService extends Reloadable {
         return methods;
     }
 
+    protected void query(String sql, RowCallbackHandler handler) {
+        query(sql, null, handler);
+    }
+
+    protected void query(String sql, Object[] args, RowCallbackHandler handler) {
+        jdbcTemplate.query(sql, args, handler);
+    }
+
+    protected KeyHolder insert(PreparedStatementCreator psc) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(psc, keyHolder);
+
+        return keyHolder;
+    }
+
     protected boolean update(String sql, Object[] args) {
         return jdbcTemplate.update(sql, args) >= 1;
+    }
+
+    protected int singleUpdate(String sql, Object[] args) {
+        return jdbcTemplate.update(sql, args);
+    }
+
+    protected int[] batchUpdate(String sql, List<Object[]> argsList) {
+        return jdbcTemplate.batchUpdate(sql, argsList);
+    }
+
+    protected void execute(TransactionCallback callback) {
+        transactionTemplate.execute(callback);
     }
 }
