68c1e26f595d11adac136f6c87ff5bf6cafedc9d
吴秋锋 <arthas.wuqf@gmail.com>
2016-02-16 16:13:25 +0800

    rename method

--- core/src/main/java/cn/momia/common/core/util/MomiaUtil.java
+++ core/src/main/java/cn/momia/common/core/util/MomiaUtil.java
@@ -7,13 +7,13 @@ import java.util.regex.Pattern;
 public class MomiaUtil {
     private static final Pattern MOBILE_PATTERN = Pattern.compile("^1[0-9]{10}$");
 
-    public static boolean isInvalid(String mobile) {
+    public static boolean isInvalidMobile(String mobile) {
         if (StringUtils.isBlank(mobile)) return true;
         return !MOBILE_PATTERN.matcher(mobile).find();
     }
 
-    public static String encrypt(String mobile) {
-        if (isInvalid(mobile)) return "";
+    public static String encryptMobile(String mobile) {
+        if (isInvalidMobile(mobile)) return "";
         return mobile.substring(0, 3) + "****" + mobile.substring(7);
     }
 }
