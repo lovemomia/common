e21b751cbe78c4e638e7b67bf391497dd4601157
吴秋锋 <arthas.wuqf@gmail.com>
2016-02-16 16:15:24 +0800

    rm sex util

--- core/src/main/java/cn/momia/common/core/util/MomiaUtil.java
+++ core/src/main/java/cn/momia/common/core/util/MomiaUtil.java
@@ -1,11 +1,15 @@
 package cn.momia.common.core.util;
 
+import com.google.common.collect.Sets;
 import org.apache.commons.lang3.StringUtils;
+import org.jsoup.helper.StringUtil;
 
+import java.util.Set;
 import java.util.regex.Pattern;
 
 public class MomiaUtil {
     private static final Pattern MOBILE_PATTERN = Pattern.compile("^1[0-9]{10}$");
+    private static final Set<String> SEX = Sets.newHashSet("男", "女");
 
     public static boolean isInvalidMobile(String mobile) {
         if (StringUtils.isBlank(mobile)) return true;
@@ -15,5 +19,9 @@ public class MomiaUtil {
     public static String encryptMobile(String mobile) {
         if (isInvalidMobile(mobile)) return "";
         return mobile.substring(0, 3) + "****" + mobile.substring(7);
+    }
+
+    public static boolean isInvalidSex(String sex) {
+        return StringUtil.isBlank(sex) || !SEX.contains(sex);
     }
 }
